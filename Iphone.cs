5356d3779b427d0effb1322b85dcc1819413d72e
is13099 <is13099@B206-12>
2023-03-01 09:03:50 +0700

    Добавьте файлы проекта.

---
+++ Iphone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabota
+{
+    internal class Iphone : Mobile
+    {
+        private string _ios_version;
+        public string Ios_version;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Model}, {Colour}, {Datapok}, {Price}, {Guarantee}, {Ios_version}");
+        }
+    }
+}
---
+++ Mobile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabota
+{
+    internal class Mobile
+    {
+        //свойства
+        private string _model; //модель телефона
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        private string _colour; //цвет телефона
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+        private DateTime _datapok; //дата покупки телефона
+        public DateTime Datapok
+        {
+            get { return _datapok; }
+            set { _datapok = value; }
+        }
+        private int _price; //цена телефона
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        private int _guarantee; //гарантия
+        public int Guarantee
+        {
+            get { return _guarantee; }
+            set { _guarantee = value; }
+        }
+
+        //методы
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Model}, {Colour}, {Datapok}, {Price}, {Guarantee}");
+        }
+
+        public void PriceCheck()
+        {
+            if (Price > 30000)
+            {
+                Console.WriteLine("Такое не потянем");
+            }
+            else
+            {
+                Console.WriteLine("Такое потянем");
+            }
+        }
+
+        public void GuaranteeCheck()
+        {
+            int year = Datapok.Year;
+            int c_year = DateTime.Now.Year;
+            if (c_year - year > Guarantee)
+            {
+                Console.WriteLine("Гарантия истекла");
+            }
+            else
+            {
+                Console.WriteLine("Гарантия не истекла");
+            }
+        }
+    }
+}
